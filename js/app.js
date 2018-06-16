f8a9130875b248ecb3043ecf1ef506a9ef2b2929
Vid M <vid.majkus@gmail.com>
2018-06-16 13:21:20 +0200

    Console.log the ingredients

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Make a get request for testna naloga
+axios.get('https://world.openfoodfacts.org/api/v0/product/737628064502.json')
+  .then(function (response) {
+  		//Save response to ingredients variable
+    	var ingredients = response.data.product.ingredients;
+    	//For each ingredients object
+    	Object.keys(ingredients).forEach(function(ingredient) {
+    		//Console log text field from each ingredient
+  			console.log(ingredients[ingredient].text);
+
+		});
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
