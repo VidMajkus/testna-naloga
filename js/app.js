8341cfd38a7544c5ddecc0705e4bf2f3ac0d7828
Vid M <vid.majkus@gmail.com>
2018-06-17 19:40:20 +0200

    slick slider

--- js/app.js
+++ js/app.js
@@ -14,3 +14,4 @@ axios.get('https://world.openfoodfacts.org/api/v0/product/737628064502.json')
   .catch(function (error) {
     console.log(error);
   });
+
---
+++ js/init.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+      $('.first-slider').slick({
+          infinite: true,
+  		  slidesToShow: 1,
+          slidesToScroll: 1
+      });
+    });
+
+$(document).ready(function(){
+      $('.second-slider').slick({
+          infinite: true,
+  		  slidesToShow: 1,
+          slidesToScroll: 1
+      });
+    });
