cfbb214810c7cc95b8cac0c6e8378f94ff9a5537
Vid M <vid.majkus@gmail.com>
2018-06-18 19:18:04 +0200

    slider arrows

--- js/app.js
+++ js/app.js
@@ -15,3 +15,5 @@ axios.get('https://world.openfoodfacts.org/api/v0/product/737628064502.json')
     console.log(error);
   });
 
+  
+
--- js/init.js
+++ js/init.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-      $('.first-slider').slick({
-          infinite: true,
-  		  slidesToShow: 1,
-          slidesToScroll: 1
-      });
-    });
+
+$('.first-slider').slick({
+  arrows: false,
+       autoplay: true,
+         speed:300,
+         arrows: false,
+  		 infinite: true,
+         draggable: true,
+         dots: true,
+         slidesToShow: 1,
+         slidesToScroll: 1
+})
+
+$('.left').click(function(){
+  $('.first-slider').slick('slickPrev');
+})
+
+$('.right').click(function(){
+  $('.first-slider').slick('slickNext');
+})
 
 $(document).ready(function(){
       $('.second-slider').slick({
+      	  autoplay: true,
+      	  speed:300,
           infinite: true,
   		  slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
       });
     });
